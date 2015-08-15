952746363ad3c8091b5c2dae92b46ec7ad56fc53
darkunz <darwin.medillo@gmail.com>
2015-08-15 20:57:22 +0800

    cps class update

--- api/app/Http/Controllers/CPS.php
+++ api/app/Http/Controllers/CPS.php
@@ -3,35 +3,38 @@ use CPS_Simple;
 use CPS_Connection;
 use CPS_LoadBalancer;
 
+/**
+ * Class CPS
+ */
 class CPS
 {
-    public $connection;
+	/** @var CPS_Simple */
+    private static $_connection;
 
-    public function connect(){
-    	if(empty($this->connection)){
-	    	$connectionStrings = array(	
-			  'tcp://cloud-us-0.clusterpoint.com:9007',	
-			  'tcp://cloud-us-1.clusterpoint.com:9007',	
-			  'tcp://cloud-us-2.clusterpoint.com:9007',	
-			  'tcp://cloud-us-3.clusterpoint.com:9007',	
-			);	
+	/**
+	 * @return \CPS_Simple
+	 */
+	public static function instance()
+	{
+		if (empty(static::$_connection)) {
+			$connectionStrings = array(
+				'tcp://cloud-us-0.clusterpoint.com:9007',
+				'tcp://cloud-us-1.clusterpoint.com:9007',
+				'tcp://cloud-us-2.clusterpoint.com:9007',
+				'tcp://cloud-us-3.clusterpoint.com:9007',
+			);
 			$cpsConn = new CPS_Connection(
-				new CPS_LoadBalancer($connectionStrings), 
-				'hurryapp', 
-				'developer@ideasmaker.net', 
-				'island123!', 
-				'document', 
-				'//document/id', 
-				array('account' => 101125));	
+				new CPS_LoadBalancer($connectionStrings),
+				'hurryapp',
+				'developer@ideasmaker.net',
+				'island123!',
+				'document',
+				'//document/id',
+				array('account' => 101125));
 			//$cpsConn->setDebug(true);	
 			$cpsSimple = new CPS_Simple($cpsConn);
-			$this->connection = $cpsSimple;		
-			return true;
-		}else{
-			return false;
+			static::$_connection = $cpsSimple;
 		}
-
-		
-    }
-
+		return static::$_connection;
+	}
 }
