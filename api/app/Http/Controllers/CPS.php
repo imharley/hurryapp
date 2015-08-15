c5ee676a66003b341f6a3e6972a4625fd75e90e8
darkunz <darwin.medillo@gmail.com>
2015-08-15 22:57:34 +0800

    CPS helpers

--- api/app/Http/Controllers/CPS.php
+++ api/app/Http/Controllers/CPS.php
@@ -1,7 +1,4 @@
 <?php
-use CPS_Simple;
-use CPS_Connection;
-use CPS_LoadBalancer;
 
 /**
  * Class CPS
@@ -36,5 +33,61 @@ class CPS
 			static::$_connection = $cpsSimple;
 		}
 		return static::$_connection;
+	}
+
+	/**
+	 * @param $conditions
+	 * @param array $options
+	 * @return object
+	 */
+	public static function findOne($type, $conditions = [], $options = [])
+	{
+		$options = $options + [
+			'list' => null,
+			'ordering' => null,
+			'returnType' => DOC_TYPE_ARRAY,
+			'stemlang' => null,
+		];
+		$query = static::build($type, $conditions);
+		$documents = CPS::instance()->search($query, 0, 1, $options['list'], $options['ordering'], $options['returnType'], $options['stemlang']);
+		if ($documents) {
+			return array_pop($documents);
+		}
+		return null;
+	}
+
+	/**
+	 * @param $type
+	 * @param $conditions
+	 * @param array $options
+	 * @return string
+	 */
+	protected static function build($type, $conditions)
+	{
+		$query = CPS_Term($type, 'document_type');
+		foreach ($conditions as $field => $value) {
+			$query .= CPS_Term($value, $field);
+		}
+		return $query;
+	}
+
+	/**
+	 * @param string $type
+	 * @param $conditions
+	 * @param array $options
+	 */
+	public static function findMany($type, $conditions = [], $options = [])
+	{
+		$options = $options + [
+			'list' => null,
+			'ordering' => null,
+			'returnType' => DOC_TYPE_ARRAY,
+			'stemlang' => null,
+			'offset' => 0,
+			'docs' => null,
+		];
+		$query = static::build($type, $conditions);
+		$documents = CPS::instance()->search($query, $options['offset'], $options['docs'], $options['list'], $options['ordering'], $options['returnType'], $options['stemlang']);
+		return $documents ? array_values($documents) : null;
 	}
 }
