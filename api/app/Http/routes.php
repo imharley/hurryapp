b768fad0c39eb8ad58e7dde457ac6a5563643311
Harley Lovitos <harley@ideasmaker.net>
2015-08-15 20:49:08 +0800

    Add store register.

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -11,6 +11,23 @@
 |
 */
 
+Route::get('/store',function()
+{
+    return view('store.index');
+});
+Route::get('/login',function()
+{
+    return view('store.login');
+});
+Route::get('/register',function()
+{
+    return view('store.register');
+});
+Route::post('/register','store@add');
+Route::post('/login','store@login');
+
+Route::get('/connect','CPS@connect');
+
 Route::get('/', function () {
     return view('welcome');
 });
